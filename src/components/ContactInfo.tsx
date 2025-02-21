c038e370a7191b1d2252f12b52dfc649b424aa5f
YesayaSitompul <2381050@unai.edu>
2025-02-21 10:54:04 +0700

    commit

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -2,7 +2,7 @@ const ContactInfo = () => {
     return (
         <div>
             <p>Email: 2381050@unai.edu</p>
-            <p>Phone: +62-823-6137-9018</p>
+            <p>Phone: +62-234-8782-1188</p>
             <p>Address:  Unai, Bandung, Indonesia</p>
         </div>
     )
